757aa4cd79837888b2e0070e35cc850b5e4ace0c
mrCodeScripts <kqlyshock@gmail.com>
2025-11-04 10:07:42 +0800

    done

--- main.cpp
+++ main.cpp
@@ -5,8 +5,33 @@
 #include <vector>
 #include <utility>
 #include <windows.h>
+#include <conio.h>
 
-int main () {
+void snakeControls(std::pair<int, int> &snakeDir)
+{
+    if (_kbhit)
+    {
+        char k = _getch();
+        switch (k)
+        {
+        case 'w':
+            snakeDir = {-1, 0};
+        case 's':
+            snakeDir = {1, 0};
+        case 'a':
+            snakeDir = {-1, 0};
+        case 'd':
+            snakeDir = {1, 0};
+        }
+    }
+}
+
+void updateSnake(std::vector<std::pair<int, int>> &snakeSeg)
+{
+}
+
+int main()
+{
 
     SetConsoleOutputCP(CP_UTF8);
 
@@ -15,22 +40,32 @@ int main () {
     int mapHeight = 10;
 
     std::pair<int, int> playerPos = {int(mapWidth / 2), int(mapHeight / 2)};
+    std::vector<std::pair<int, int>> snake;
+    std::vector<std::pair<int, int>> snakeSeg = {
+        {int(mapWidth / 2), int(mapHeight / 2)},
+        {int(mapWidth / 2), int(mapHeight / 2) - 1},
+        {int(mapWidth / 2), int(mapHeight / 2) - 2},
+    };
+    std::pair<int, int> snakeDir = {1, 0};
 
     char block = ' ';
 
-    std::pair<int, int> snakeDir = {0, 1};
-    
-    while (animate) {
+    while (animate)
+    {
 
-        // Clear the console (platform dependent)
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+// Clear the console (platform dependent)
+#ifdef _WIN32
+        system("cls");
+#else
+        system("clear");
+#endif
 
-        for (int i = 0; i < mapHeight; i++) {
-            for (int j = 0; j < mapWidth; j++) {
+        snakeControls(snakeDir);
+
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
                 /*
                 if (i == 0 || i == mapHeight) {
                     if (i == 0 && j == 0) std::cout << u8"╔";
@@ -41,21 +76,44 @@ int main () {
                 }
                 */
 
-                if (i == 0) {
-                    if (j == 0) { 
-                        std::cout << u8"╔";
-                    } else if (j == mapWidth - 1) {
-                        std::cout << u8"╗";
-                    } else std::cout << u8"═";
-                } else if (i == mapHeight - 1) {
-                    if (j == 0) {
-                        std::cout << u8"╚";
-                    } else if (j == mapWidth - 1) {
-                        std::cout << u8"╝";
-                    } else std::cout << u8"═";
-                } else {
-                    if (j == 0 || j == mapWidth - 1) std::cout << u8"║";
-                    else std::cout << block;
+                if ()
+                {
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        if (j == 0)
+                        {
+                            std::cout << u8"╔";
+                        }
+                        else if (j == mapWidth - 1)
+                        {
+                            std::cout << u8"╗";
+                        }
+                        else
+                            std::cout << u8"═";
+                    }
+                    else if (i == mapHeight - 1)
+                    {
+                        if (j == 0)
+                        {
+                            std::cout << u8"╚";
+                        }
+                        else if (j == mapWidth - 1)
+                        {
+                            std::cout << u8"╝";
+                        }
+                        else
+                            std::cout << u8"═";
+                    }
+                    else
+                    {
+                        if (j == 0 || j == mapWidth - 1)
+                            std::cout << u8"║";
+                        else
+                            std::cout << block;
+                    }
                 }
             }
             std::cout << std::endl;
